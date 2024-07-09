a1ffa47b24018142269cc359fe0f42f6cc754075
Guilherme Apolinario <131500489+gtellapolinario@users.noreply.github.com>
2024-07-09 16:07:49 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,52 +1,60 @@
-# https://github.com/gtellapolinario/Text-to-voice/streamlit_app.py
-# Aplicativo streamlit text to speech openai 
 import os
 import tempfile
 import openai
 import requests
 import streamlit as st
 
-st.sidebar.title("Sobre o Projeto")
-st.sidebar.info("""
-            Este projeto foi desenvolvido por Dr. Guilherme Apolinário, médico com interesse em tecnologia aplicada à saúde.
-            - **GitHub:** [Link do projeto](https://github.com/gtellapolinario/Text-to-voice)
-            - [Obsidian-Publish](https://dr-guilhermeapolinario.com)
-            - [Exemplos de gravação](https://dr-guilhermeapolinario.com/2.+%C3%81reas/Aprendizado/Exemplos+de+voz+api+Openai)
-                """)
-
-st.sidebar.title("Informações")
-st.sidebar.info(""" 
-            Aplicativo de conversão direta de texto para áudio, utilizando a API da OpenAI. 
-            É necessário inserir a chave da API que você copia em sua conta OpenAI. 
-            Para copiar a chave acesso o site: [OpenAI API Keys](https://platform.openai.com/api-keys)
-            """)
-
-
 st.title('#### 🤖💬 Conversor de Texto em Áudio OpenAI')
+
+# Move API key input to main area
+openai_api_key = st.text_input("OpenAI API Key", type="password")
+st.markdown("[Pegue aqui sua chave OpenAI API](https://platform.openai.com/account/api-keys)")
 
 texto_usuario = st.text_area("Digite ou cole o texto aqui:", max_chars=4096)
 
-if 'OPENAI_API_KEY' in st.secrets:
-    st.success('API key already provided!', icon='✅')
-    openai.api_key = st.secrets['OPENAI_API_KEY']
-else:
-    openai.api_key = st.text_input('Digite sua chave de API:', type='password')
+# Sidebar
+with st.sidebar:
+    model_selection = st.radio("Qualidade:", ("tts-1", "tts-1-hd"))
+    if st.button("Reiniciar"):
+        st.session_state["messages"] = [
+            {"role": "assistant", "content": "How can I help you?"}
+        ]
+        st.experimental_rerun()
+
+st.sidebar.title("Sobre o Projeto")
+st.sidebar.markdown("""
+                 Conversor de texto em áudio com a api Openai.
+            #
+            - **GitHub:** [Link do projeto](https://github.com/gtellapolinario/Text-to-voice)
+            ######
+            - [Obsidian-Publish](https://dr-guilhermeapolinario.com)
+            ######
+            - [Exemplos de gravação](https://dr-guilhermeapolinario.com/2.+%C3%81reas/Aprendizado/Exemplos+de+voz+api+Openai)
+            *by* Dr. Guilherme Apolinário
+                
+                """)
 
 velocidade_voz = st.slider("Velocidade da voz:", 0.25, 4.0, 1.0)
 vozes_disponiveis = ["alloy", "echo", "fable", "onyx", "nova", "shimmer"]
 
-client = openai.api_key
+# Set API key
+if openai_api_key:
+    openai.api_key = openai_api_key
+    st.success('API key provided!', icon='✅')
+else:
+    st.warning('Please enter your OpenAI API key.', icon='⚠️')
+
 # Função para converter texto em áudio
 def converter_texto_em_audio(voice):
     if texto_usuario and openai.api_key:
-        response = openai.Audio.create(
-            model="tts-1",
-            voice=voice,
-            input=texto_usuario,
-            speed=velocidade_voz
-        )
-        # Verifica se a resposta é bem-sucedida e reproduz o áudio
-        if response.status_code == 200:
+        try:
+            response = openai.Audio.create(
+                model=model_selection,
+                voice=voice,
+                input=texto_usuario,
+                speed=velocidade_voz
+            )
+            # Verifica se a resposta é bem-sucedida e reproduz o áudio
             audio_bytes = response.content
             if len(audio_bytes) > 0:
                 with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as fp:
@@ -65,6 +73,8 @@ def converter_texto_em_audio(voice):
                 os.unlink(fp.name)  # Clean up the temporary file
             else:
                 st.error("Não foi possível gerar o áudio. Por favor, tente novamente.")
+        except Exception as e:
+            st.error(f"An error occurred: {str(e)}")
 
 # Botões para seleção de voz
 for voz in vozes_disponiveis:
@@ -72,5 +82,3 @@ for voz in vozes_disponiveis:
 
 # Link para amostras de voz
 st.markdown("Confira as [amostras de voz](https://platform.openai.com/docs/guides/text-to-speech) disponíveis.")
-
-
